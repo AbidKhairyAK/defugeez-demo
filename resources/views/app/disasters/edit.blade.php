610b6779001e413a04e319f1981e14d41d5ea148
dammahum <muhammadsirajuddin39@gmail.com>
2018-11-20 20:46:53 +0700

    Disaster CRUD

---
+++ resources/views/app/disasters/edit.blade.php
@@ -0,0 +1,35 @@
+@extends('app.layouts.app')
+
+@section('title', 'Tambah Bencana')
+
+@section('content')
+<div id="main" class="container">
+	<div class="section">
+
+		<div class="section-separator">
+			<hr class="hr-thick">
+			<hr class="hr-thin">
+			<h3>Daftar Bencana</h3>
+		</div>
+
+		<div class="section-wrapper col-sm-12 rounded bg-light shadow">
+
+			<h5 class="section-title">Edit Daftar Bencana</h5>
+
+			<div class="section-content">
+				{!! Form::model($disaster, [
+					'method' => 'PUT',
+					'route' => ['disasters.update', $disaster->id],
+					'id'		=> 'disaster-form'
+				]) !!}
+
+				@include('app.disasters.form')
+
+				{!! Form::close() !!}
+			</div>
+
+		</div>
+
+	</div>
+</div>
+@endsection
