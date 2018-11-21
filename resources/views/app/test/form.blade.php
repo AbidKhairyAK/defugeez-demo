b8b7d79171dc8510dec54dc44b56c19b944784f9
AbidKhairyAK <abid.khairy.ak@gmail.com>
2018-11-21 07:15:58 +0700

    Leaflet Marker

--- resources/views/app/test/form.blade.php
+++ resources/views/app/test/form.blade.php
@@ -1,3 +1,12 @@
+
+@section('style')
+<!-- Leaflet -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
+ integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+ crossorigin=""/>
+<link rel="stylesheet" href="//unpkg.com/leaflet-gesture-handling/dist/leaflet-gesture-handling.min.css" type="text/css">
+@endsection
+
 @php
 	$error_email = [
 		'invalid' => $errors->has('email') ? 'is-invalid' : '',
@@ -80,5 +89,67 @@
 	{!! $error_desc['feedback'] !!}
 </div>
 
+<div id="map-wrapper" class="section-wrapper col-sm-12 rounded bg-light shadow">
+	<div id="map"></div>
+</div>
+
+<div class="form-group">
+	{!! Form::label('lat', 'Latitude :', ['class' => 'label']) !!}
+	<span class="small text-muted"> (optional)</span>
+	{!! Form::text('lat', null, ['class' => 'form-control', 'id' => 'lat', 'rows' => '5', 'required']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('lng', 'Longitude :', ['class' => 'label']) !!}
+	<span class="small text-muted"> (optional)</span>
+	{!! Form::text('lng', null, ['class' => 'form-control', 'id' => 'lng', 'rows' => '5', 'required']) !!}
+</div>
+
+
 <a href="#" class="btn btn-secondary px-5">Cancel</a>
 <button type="submit" class="btn btn-primary px-5">Submit</button>
+
+@section('script')
+<!-- Leaflet -->
+<script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
+ integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
+ crossorigin=""></script>
+<!-- Leaflet Provider -->
+<script src="/js/leaflet-provider.js"></script>
+<script src="//unpkg.com/leaflet-gesture-handling"></script>
+<script type="text/javascript">
+  var mymap = L.map('map', {
+    center: [-1.090675, 114.873782],
+    zoom: 11,
+    gestureHandling: true,
+  });
+
+  var map =  L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  });
+
+    L.marker([-1.090675, 114.713782]).addTo(mymap)
+      .bindPopup(`<b>Posko Krapyak</b><br/><a href="{{ url('/refugees') }}">Lebih Lengkap!</a>.`);
+
+    L.marker([-1.010675, 114.953782]).addTo(mymap)
+      .bindPopup(`<b>Posko Krapyak</b><br/><a href="{{ url('/refugees') }}">Lebih Lengkap!</a>.`);
+
+    L.marker([-1.151675, 114.893782]).addTo(mymap)
+      .bindPopup(`<b>Posko Krapyak</b><br/><a href="{{ url('/refugees') }}">Lebih Lengkap!</a>.`).openPopup();
+
+  map.addTo(mymap);
+
+
+  mymap.on('click', function(e){
+    var lat = e.latlng.lat;
+    var lng = e.latlng.lng;
+    
+    L.marker([lat, lng]).addTo(mymap)
+      .bindPopup("<b>Bencana Baru</b>.").openPopup();
+
+    $('#lat').val(lat);
+    $('#lng').val(lng);
+  });
+</script>
+@endsection
